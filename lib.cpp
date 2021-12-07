73d6f8579c9ebacce96455acc142d30fbacae6c4
Francesco <91691097+5291353@users.noreply.github.com>
2021-12-07 17:20:30 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,17 @@
-
+#include <iostream>
+#include "lib.h"
+using namespace std;
+int maggiore(int numeri[],int dim){
+    int b=0,num=0;
+    for(int i=0;i<dim;i++){
+        cin>>num;
+        numeri[i]=num;
+    }
+    b=numeri[0];
+    for(int j=0;j<dim;j++){
+        if(b<numeri[j]){
+            b=numeri[j];
+        }
+    }
+    return b;
+}
