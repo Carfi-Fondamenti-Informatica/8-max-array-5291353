64500e167b8a4f221914a274fca60ebda5f193b3
Francesco <91691097+5291353@users.noreply.github.com>
2021-12-07 18:04:38 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,11 +6,12 @@ int main() {
     int a=0,num=0;
     cin>> a;
     int numeri[a];
-
-    for(int i=0;i<a;i++){
-        num=0;
-        cin>>num;
-        numeri[i]=num;
+    if(a>0) {
+        for (int i = 0; i < a; i++) {
+            num = 0;
+            cin >> num;
+            numeri[i] = num;
+        }
     }
     cout<<maggiore(numeri,a)<<endl;
     return 0;
