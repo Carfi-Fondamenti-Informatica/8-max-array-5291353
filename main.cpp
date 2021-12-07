0ebada67eabd9f8ab1c3f528af70189c8a27df64
Francesco <91691097+5291353@users.noreply.github.com>
2021-12-07 17:21:25 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+    int a=0,numeri[a];
+    cin>> a;
+    cout<<maggiore(numeri,a)<<endl;
+    return 0;
 }
