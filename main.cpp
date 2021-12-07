6acc45455b7d17ea47aa8a678888ae8c6f86a00c
Francesco <91691097+5291353@users.noreply.github.com>
2021-12-07 17:58:36 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,8 +3,16 @@
 using namespace std;
 
 int main() {
-    int a=0,numeri[a];
+    int a=0,num=0;
     cin>> a;
+    int numeri[a];
+
+    for(int i=0;i<a;i++){
+        num=0;
+        cin>>num;
+        numeri[i]=num;
+    }
     cout<<maggiore(numeri,a)<<endl;
     return 0;
 }
+
